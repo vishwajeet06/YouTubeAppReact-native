c1ad35811e35462ae61756828a571c3b154b86d5
vishwajeet06 <vishwajeetkcse@gmail.com>
2021-06-08 16:50:49 +0530

    y

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Youtube App Frontend</Text>
+      <Text>Youtube App Frontend hybrid</Text>
       <StatusBar style="auto" />
     </View>
   );
